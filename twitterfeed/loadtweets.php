838aa01a86be59cf5111c318c3bd444bece290b0
Nik <niksudan@gmail.com>
2014-06-30 16:03:55 +0100

    Add support for search queries

--- twitterfeed/loadtweets.php
+++ twitterfeed/loadtweets.php
@@ -7,6 +7,15 @@
 	$access_secret = '';
 
 	$connection = new TwitterOAuth($api_key, $api_secret, $access_key, $access_secret);
-	$tweets = json_encode($connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twtr_user."&count=".$twtr_count));
+
+	switch ($GLOBALS['twtr_type']) {
+		case 'user':
+			$twtr_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twtr_input."&count=".$twtr_count;
+			break;
+		case 'search':
+			$twtr_url = "https://api.twitter.com/1.1/search/tweets.json?q=".$twtr_input."&count=".$twtr_count;
+			break;
+	}
+
+	$tweets = json_encode($connection->get($twtr_url));
 	return $tweets;
-?>
