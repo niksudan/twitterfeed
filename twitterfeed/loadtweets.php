9dcd6cf99fb2b6c71ee7f20eef51323a5963cc3d
Nik <niksudan@gmail.com>
2014-07-02 13:43:42 +0100

    Update to v1.2

--- twitterfeed/loadtweets.php
+++ twitterfeed/loadtweets.php
@@ -1,21 +1,32 @@
 <?php
-	require_once('twitteroauth.php');
+require_once('twitteroauth.php');
 
-	$api_key = '';
-	$api_secret = '';
-	$access_key = '';
-	$access_secret = '';
+// Add your keys here
 
-	$connection = new TwitterOAuth($api_key, $api_secret, $access_key, $access_secret);
+$api_key = '';
+$api_secret = '';
+$access_key = '';
+$access_secret = '';
 
-	switch ($GLOBALS['twtr_type']) {
-		case 'user':
-			$twtr_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twtr_input."&count=".$twtr_count;
-			break;
-		case 'search':
-			$twtr_url = "https://api.twitter.com/1.1/search/tweets.json?q=".$twtr_input."&count=".$twtr_count;
-			break;
-	}
+$connection = new TwitterOAuth($api_key, $api_secret, $access_key, $access_secret);
 
-	$tweets = json_encode($connection->get($twtr_url));
-	return $tweets;
+switch ($GLOBALS['twtr_type']) {
+
+	case 'user':
+		$twtr_url = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twtr_option[0]."&count=".$twtr_option[1];
+		break;
+
+	case 'search':
+		$GLOBALS['twtr_srch'] = $twtr_option[0];
+		$twtr_url = "https://api.twitter.com/1.1/search/tweets.json?q=".$twtr_option[0]."&count=".$twtr_option[1];
+		break;
+
+	default:
+		$GLOBALS['twtr_type'] = 'search';
+		$GLOBALS['twtr_srch'] = $twtr_option[0];
+		$twtr_url = "https://api.twitter.com/1.1/search/tweets.json?q=".$twtr_option[0]."&count=".$twtr_option[1];
+		break;
+}
+
+$tweets = json_encode($connection->get($twtr_url));
+return $tweets;
