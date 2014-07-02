42ef416382aa327dc9fa67e3a4915ea55bf2ac47
Nik <niksudan@gmail.com>
2014-07-02 14:58:00 +0100

    Update to v1.2

--- example.php
+++ example.php
@@ -1,9 +1,9 @@
-<?php require_once('twitterfeed/twitterfeed.php'); ?>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Twitter Feed</title>
+	<?php require_once('twitterfeed/twitterfeed.php'); ?>
 </head>
 
 <body>
