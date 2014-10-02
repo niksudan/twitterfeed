b9661b6676cb740f2e7f6949c8042e634d882206
Nik <niksudan@gmail.com>
2014-10-02 08:37:05 +0100

    v1.4 support

--- tweets.php
+++ tweets.php
@@ -1,42 +1,32 @@
 <ul class="tweets">
-
-	<!-- List of tweets -->
-
-	<?php foreach ($GLOBALS['tweets'] as $GLOBALS['tweet']) : ?>
-
+	<?php foreach ($twitter->tweets as $twitter->tweet) : ?>
 		<li>
-
 			<hr>
 			<p>
 				<small>
-					<?php if (tf_isRT()) {echo 'RT - ';} ?>
-					<?php tf_author('name') ?> 
-					<a href="<?php tf_authorURL(); ?>">
-						@<?php tf_author('screen_name'); ?>
+					<?php if ($twitter->isRT()) {echo 'RT - ';} ?>
+					<?php $twitter->author('name') ?> 
+					<a href="<?php $twitter->authorURL(); ?>">
+						@<?php $twitter->author('screen_name'); ?>
 					</a>
-					<span><br><?php tf_user('created_at'); ?></span>
+					<span><br><?php $twitter->tweet('created_at'); ?></span>
 				</small>
 			</p>
-			
 			<div class="avatar">
-				<img src="<?php tf_avatar(); ?>" width="50px">
+				<img src="<?php $twitter->authorAvatar(); ?>" width="50px">
 			</div>
-
 			<div style="margin-left: 60px;">
-				<p><?php tf_tweetText(); ?></p>
+				<p><?php $twitter->tweetHTML(); ?></p>
 				<p>
 					<span><small>
-						<?php tf_tweet('retweet_count'); ?> Retweets, 
-						<?php tf_tweet('favorite_count'); ?> Favourites
+						<?php $twitter->tweet('retweet_count'); ?> Retweets, 
+						<?php $twitter->tweet('favorite_count'); ?> Favourites
 					</small></span>
 					<small style="float: right">
-						<a href="<?php tf_tweetURL(); ?>">Link</a>
+						<a href="<?php $twitter->tweetURL(); ?>">Link</a>
 					</small>
 				</p>
 			</div>
-
 		</li>
-
 	<?php endforeach; ?>
-
 </ul>
