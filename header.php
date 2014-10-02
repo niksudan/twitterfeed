f91c562f53afabbe68eef9c68e68b89f0e4b6ea0
Nik <niksudan@gmail.com>
2014-10-02 08:36:40 +0100

    v1.4 support

--- header.php
+++ header.php
@@ -1,33 +1,19 @@
-<?php if (tf_isType('user')) : ?>
-
-	<!-- User Profile Header -->
-
-	<div class="header" style="background: url('<?php tf_user('profile_banner_url'); ?>/1500x500');">
-
-		<img src="<?php tf_avatar(); ?>" width="100px" style="float: right;">
-
-		<h1><?php tf_user('name'); ?>'s Timeline</h1>
-
+<?php if ($twitter->isType('user')) : ?>
+	<div class="header" style="background: url('<?php $twitter->user('profile_banner_url'); ?>/1500x500');">
+		<div class="avatar">
+			<img src="<?php $twitter->userAvatar(); ?>">
+		</div>
+		<h1><?php $twitter->user('name'); ?>'s Timeline</h1>
 		<p>
 			<small>
-				Followers: <?php tf_user('followers_count'); ?>
-				<br>Following: <?php tf_user('friends_count'); ?>
-				<br>Tweets: <?php tf_user('statuses_count'); ?>
+				Followers: <?php $twitter->user('followers_count'); ?>
+				<br>Following: <?php $twitter->user('friends_count'); ?>
+				<br>Tweets: <?php $twitter->user('statuses_count'); ?>
 			</small>
 		</p>
-
 	</div>
-
 <?php else : ?>
-
-	<!-- Search Header -->
-
 	<div class="header">
-
-		<h1>'<?php tf_searchQuery(); ?>' Tweets</h1>
-
+		<h1>'<?php $twitter->searchQuery(); ?>' Tweets</h1>
 	</div>
-
-
-
 <?php endif; ?>
